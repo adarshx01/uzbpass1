9c8a4184f9791b4e19a62e393becd7a7e7b2a7d9
Advait <76993204+777advait@users.noreply.github.com>
2024-08-09 20:20:19 +0530

    add &apos;

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -46,7 +46,7 @@ export default function Dashboard() {
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="underline">
               Sign up
             </Link>
